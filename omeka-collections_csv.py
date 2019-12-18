830cb2559261db2e50727ca2921d12dde85683c7
luisfeliperd <luisfelipeprf@gmail.com>
2019-12-18 12:02:58 -0300

    Create omeka-collections_csv.py

---
+++ omeka-collections_csv.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import requests
+from collections import defaultdict
+import pandas as pd
+import time
+
+
+# In[5]:
+
+
+endpoint_collections = 'http://bdce.unb.br/api/collections'
+response = requests.get(endpoint_collections)
+collections = response.json()
+metadatum_dict = {'Date':[], 'Creator':[], 'Publisher':[], 'Title':[], 'Subject':[]}
+collection_dict = defaultdict(list)
+collection_dict.update(metadatum_dict)
+
+
+# In[6]:
+
+
+for collection in collections:
+    
+    metadata_dict = {'Date':[], 'Creator':[], 'Publisher':[], 'Title':[], 'Subject':[]}
+    
+    collection_dict['id'].append(collection['id'])
+    collection_dict['itens_count'].append(collection['items']['count'])
+    
+    for metadata in collection['element_texts']:
+        
+        for key in metadata_dict.keys():
+            
+            if key == metadata['element']['name']:
+                metadata_dict[key].append(metadata['text'])
+            else:
+                metadata_dict[key].append('')
+        
+    for key in metadata_dict.keys():
+        
+        metadata_dict[key] = list(filter(None, metadata_dict[key]))
+        
+        if metadata_dict[key]:
+            collection_dict[key].append("||".join(metadata_dict[key]))
+        else:
+            collection_dict[key].append("".join(metadata_dict[key]))
+
+
+# In[7]:
+
+
+export_df = pd.DataFrame(collection_dict)
+export_df.to_csv('collections_omeka_export.csv')
+
+
+# In[ ]:
+
+
+
+
