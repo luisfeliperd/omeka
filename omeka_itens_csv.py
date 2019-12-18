178a7d664fc84cc0a5694edfb569a6c999748d97
luisfeliperd <luisfelipeprf@gmail.com>
2019-12-18 15:04:41 -0300

    Update omeka_itens_csv.py

--- omeka_itens_csv.py
+++ omeka_itens_csv.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import requests
 from collections import defaultdict
 import pandas as pd
 import time
-
-
-# In[2]:
-
 
 endpoint_items = 'http://bdce.unb.br/api/items'
 endpoint_collections = 'http://bdce.unb.br/api/collections'
@@ -20,10 +13,6 @@ metadata_dict = defaultdict(list)
 metadatum_dict = {'Coordenadora do Curso de Comunicação Organizacional':[],'Diretor de Marketing':[],'Diagramador(es)':[],'Repórteres':[],'Editor ou diretor de arte':[],'Coordenadora de Laboratórios':[],'Chefe de Departamento de Comunicação Organizacional':[],'Suporte de informática':[],'Publisher':[],'Projeto de Marketing':[],'Equipe':[],'Secretário de redação':[],'Monitor(es)':[],'Projeto gráfico':[],'Revisão':[],'Chefe de Departamento de Audiovisuais e Publicidade':[],'Editor(es)':[],'Colaborador(es)':[],'Executivo de Marketing':[],'Coordenador(a) de Graduação':[],'Editor(a) de Arte':[],'Localização no CEDOC':[],'Creator':[],'Editor(a) executivo':[],'Colaboradores':[],'Revisor de arte':[],'Coordenadora de Projetos Finais':[],'Dimensões físicas':[],'Secretária de Redação':[],'Professor(es)':[],'Coordenador de Pós-Graduação':[],'Fotógrafo':[],'Secretária de Marketing':[],'Capa':[],'Localização no arquivo':[],'Impressão':[],'Coordenador(a) de Extensão':[],'Subject':[],'Editor(a) chefe':[],'Secretário(a)':[],'Revisor(es)':[],'Chefe de Departamento de Jornalismo':[],'Ilustrador(es)':[],'Title':[],'Notas':[],'Jornalista(s)':[],'Gráfica':[],'Date':[],'Editor de Fotográfia':[]}
 metadata_dict.update(metadatum_dict)
 items = response.json()
-
-
-# In[3]:
-
 
 #Extração dos Metadados
 page = 0
@@ -34,19 +23,24 @@ while requests.get(endpoint_items, params = {'page':page}).json():
     
     page+=1
     
-    print(page, end=", ")  
+    print(page, end=", ")
     
     for item in items:
         
-                                #### Coleção ####
+                                #### COLECAO E ID ####
+            
+        #Id do Item
         metadata_dict['id'].append(item['id'])
         
+        #Coleção do Item
         try:
             metadata_dict['colecao'].append(item['collection']['id'])
         except:
             metadata_dict['colecao'].append('')
             
+            
                                 ####  METADADOS  ####
+        #Dicionário com todos os metadados possíveis dos itens, mapeado previamente.
         metadatum_dict = {'Coordenadora do Curso de Comunicação Organizacional':[],'Diretor de Marketing':[],
                           'Diagramador(es)':[],'Repórteres':[],'Editor ou diretor de arte':[],
                           'Coordenadora de Laboratórios':[],'Chefe de Departamento de Comunicação Organizacional':[],
@@ -62,6 +56,7 @@ while requests.get(endpoint_items, params = {'page':page}).json():
                           'Revisor(es)':[],'Chefe de Departamento de Jornalismo':[],'Ilustrador(es)':[],'Title':[],
                           'Notas':[],'Jornalista(s)':[],'Gráfica':[],'Date':[],'Editor de Fotográfia':[]}
         
+        #Faz um dicionário que verifica os valores dos metadados disponíveis no item
         for metadata in item['element_texts']:
             
             for key in metadatum_dict.keys():
@@ -71,16 +66,17 @@ while requests.get(endpoint_items, params = {'page':page}).json():
                     
                 else:
                     metadatum_dict[key].append('')
-                    
+        
+        #Adiciona os valores do dicinário anterior ao dicionário com os valores finais.
         for key in metadatum_dict.keys():
             
             metadatum_dict[key] = list(filter(None, metadatum_dict[key]))
             
             if metadatum_dict[key]:
-                metadata_dict[key].append("||".join(metadatum_dict[key]))
+                metadata_dict[key].append("||".join(set(metadatum_dict[key])))
                 
             else:
-                 metadata_dict[key].append("".join(metadatum_dict[key]))
+                 metadata_dict[key].append("".join(set(metadatum_dict[key])))
             
                                 ##### ANEXOS #####
                 
@@ -130,8 +126,9 @@ while requests.get(endpoint_items, params = {'page':page}).json():
                 exhibit_dict['id'].append(str(exhibit['id']))
                 exhibit_dict['title'].append(exhibit['title'])
 
-        metadata_dict['exhibit_id'].append("||".join(exhibit_dict['id']))
-        metadata_dict['exhibit_name'].append("||".join(exhibit_dict['title']))
+        metadata_dict['exhibit_id'].append("||".join(set(exhibit_dict['id'])))
+        metadata_dict['exhibit_name'].append("||".join(set(exhibit_dict['title'])))
+        
         
                                     #### GEOLOCALIZAÇÕES ####
         if 'geolocations' in item['extended_resources'].keys():
@@ -148,6 +145,7 @@ while requests.get(endpoint_items, params = {'page':page}).json():
             metadata_dict['geo_longitude'].append('')
             metadata_dict['geo_adress'].append('')
     
+    
                                                 #### TAGS ####
         lista_tags = []
         
@@ -157,152 +155,14 @@ while requests.get(endpoint_items, params = {'page':page}).json():
         lista_tags = list(filter(None, lista_tags))
         
         if len(lista_tags)>1:
-            metadata_dict['tags'].append("||".join(lista_tags))
+            metadata_dict['tags'].append("||".join(set(lista_tags)))
         else:
-            metadata_dict['tags'].append("".join(lista_tags))
+            metadata_dict['tags'].append("".join(set(lista_tags)))
             
     time.sleep(20)
     
 for key in metadata_dict.keys():
     print(key, len(metadata_dict[key]))
 
-
-# In[4]:
-
-
 expt_df = pd.DataFrame(metadata_dict)
 expt_df.to_csv('itens_omeka_bdce.csv')
-
-
-# # SUBTIPOS DE METADADOS
-# 
-#          """                     #### INFOS BÁSICAS ####
-#         
-#         for value in item.keys():
-#             if type(item[value]) != dict and type(item[value]) != list:
-#                 metadata_dict[value].append(str(item[value]))
-#         """
-
-# In[ ]:
-
-
-#INFOS BÁSICAS DO ITEM (OK)
-infos_dict = {}
-
-for item in items[0]:
-    if type(items[0][item]) != dict and type(items[0][item]) != list:
-        infos_dict[item] = str(items[0][item])
-
-print(infos_dict)
-
-
-# In[ ]:
-
-
-#TAGS DO ITEM
-for tag in items[0]['tags']:
-    print(tag['name'])
-
-
-# In[ ]:
-
-
-#ANEXOS (OK)
-#O json do item direcina par outro json com os links dos arquivos anexados ao item
-lista_att = []
-
-#acessando o endpoint do json dos anexos do item
-files = requests.get(items[0]['files']['url'])
-
-#adiciona a url de cada anexo em uma lista
-for file in files.json():
-    lista_att.append(file['file_urls']['original'])
-
-#para documento principal no Tainacan
-document = lista_att[0]
-
-#para anexos no Tainacan
-if len(lista_att) >1:
-    attch = "||".join(lista_att[1:])
-else:
-    attch = ''
-print(document, attch)
-
-
-# In[ ]:
-
-
-#RELAÇÃO COM EXIBIÇÃO (OK)
-exhibit_endpoint = items[0]['extended_resources']['exhibit_pages']['url']
-exhibit = requests.get(exhibit_endpoint).json()
-
-#Id e nome da exibição
-exhibit_id = exhibit['id']
-exhibit_name = exhibit['title']
-
-print(exhibit_id, exhibit_name)
-
-
-# In[ ]:
-
-
-#GEOLOCALIZAÇÃO #Pode não ocorrer (OK)
-geolocation_endpoint = items[0]['extended_resources']['geolocations']['url']
-geolocation = requests.get(geolocation_endpoint).json()
-
-#Latitude, Longitude, Endereço
-geo_latitude = geolocation['latitude']
-geo_longitude = geolocation['longitude']
-geo_adress = geolocation['address']
-
-print(geo_latitude, geo_longitude, geo_adress)
-
-
-# In[ ]:
-
-
-#METADADOS DO ITEM (OK)
-for metadata in items[0]['element_texts']:
-    print(metadata['element']['name'], metadata['text'])
-
-
-# In[ ]:
-
-
-#ITERAR PELAS PÁGINAS DE ITENS
-page = 0
-
-while requests.get(endpoint_items, params = {'page':page, 'per_page':'5'}).json():
-    page+=1
-    print(page)
-    
-    for item in requests.get(endpoint_items, params = {'page':page, 'per_page':'5'}).json():
-
-
-# In[ ]:
-
-
-#teste
-page = 0
-
-lista_metadata = []
-
-while requests.get(endpoint_items, params = {'page':page}).json():
-    page+=1
-    print(page)
-    
-    for item in requests.get(endpoint_items, params = {'page':page}).json():        
-        
-        for metadata in item['element_texts']:
-            for key in metadatum_dict.keys():
-                if key == metadata['element']['name']:
-                    metadatum_dict[key].append(metadata['text'])
-                else:
-                    metadatum_dict[key].append('')
-
-
-# In[ ]:
-
-
-
-
